fccca872c880cf093b94c272c8e25258c55266d9
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-05 17:22:05 -0300

    wip: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file: str, instance: Queue):
+    print(range(len(instance)))
+    for i in range(len(instance)):
+        print(f"i: {i}")
+        file = instance.search(i)
+        if file["nome_do_arquivo"] == path_file:
+            return print("Arquivo já processado", file=sys.stderr)
+    file_lines = txt_importer(path_file)
+    item = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+    instance.enqueue(item)
+    print(item, file=sys.stdout)
 
 
 def remove(instance):
@@ -8,3 +26,10 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+new_queue = Queue()
+new_queue2 = Queue()
+# process("ting_file_management/example.txt", new_queue)
+process("ting_file_management/example.txt", new_queue2)
+process("ting_file_management/example.txt", new_queue2)

64c169b099036b88d499ad1b653be3e63c19dfc8
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-05 18:10:24 -0300

    feat: req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    lower_word = word.lower()
+    matches = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        data = {
+            "palavra": lower_word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        lines = file["linhas_do_arquivo"]
+        for li in range(len(lines)):
+            if lower_word in lines[li].lower():
+                data["ocorrencias"].append({"linha": li + 1})
+        if len(data["ocorrencias"]) > 0:
+            matches.append(data)
+    return matches
 
 
 def search_by_word(word, instance):

b3ebfcb88c7b9eb6122120a8ad8d446d528c35ac
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-05 16:38:21 -0300

    feat: req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            return print("Formato inválido", file=sys.stderr)
+        with open(path_file, "r") as file:
+            read_file = file.read()
+            splited_file = read_file.split("\n")
+            return splited_file
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

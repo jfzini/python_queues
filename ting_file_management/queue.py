f818705a7681a69fc136d517a2dfd87ca570c47d
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-05 16:25:12 -0300

    refactor: removing unnecessary class method

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -15,7 +15,7 @@ class Queue(AbstractQueue):
         self._queue.append(value)
 
     def dequeue(self):
-        if self.is_empty():
+        if len(self._queue) == 0:
             raise IndexError("A fila está vazia")
         return self._queue.pop(0)
 
@@ -23,6 +23,3 @@ class Queue(AbstractQueue):
         if index < 0 or index >= len(self._queue):
             raise IndexError("Índice Inválido ou Inexistente")
         return self._queue[index]
-
-    def is_empty(self):
-        return len(self._queue) == 0
